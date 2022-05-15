c7c0dfea9f0eb90e8cb2ab997be6a22035a9a8cc
Szyfr <jmanz5@hotmail.com>
2022-05-15 01:31:26 -0400

    Minor change

--- build.bat
+++ build.bat
@@ -9,8 +9,4 @@ xcopy "data\"    "target\debug\%date%\data\" /v /q /s /e /y > nul
 xcopy "src\"     "target\debug\%date%\src\"  /v /q /s /e /y > nul
 xcopy "include\" "target\debug\%date%\"      /v /q /s /e /y > nul
 
-rem ROBOCOPY "G:\FanGSOdin\data"    "G:\FanGSOdin\target\debug\%date%\data"       /mir /nfl /ndl /njh /njs /np /ns /nc > nul
-rem ROBOCOPY "G:\FanGSOdin\src"     "G:\FanGSOdin\target\debug\%date%\source\src" /mir /nfl /ndl /njh /njs /np /ns /nc > nul
-rem ROBOCOPY "G:\FanGSOdin\include" "G:\FanGSOdin\target\debug\%date%"            /mir /nfl /ndl /njh /njs /np /ns /nc >nul
-
 odin build G:\FanGS\src -out=G:\FanGS\target\debug\%date%\FanGS.exe
